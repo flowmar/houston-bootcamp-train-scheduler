51392a67bd26f9deeb7d57030041e40cb082eaff
Flowmar <omar.imam@kineticvortex.com>
2017-10-30 20:21:40 -0500

    add .babelrc

---
+++ public/question.js
@@ -0,0 +1,51 @@
+// // 2 * (1 + 10) * 30
+
+// let addResult;
+// let mResult;
+// let m1 = 2;
+// let m2 = 30;
+
+// function add(a, b, callback) {
+//     addResult = a + b;
+
+//     return addResult;
+// };
+
+// function multiply(a, b, callback) {
+//     mResult = a * b;
+
+//     return mResult
+// };
+
+// add(1, 10, multiply(a, b){
+//     mResult = (addResult * m1);
+
+//     multiply
+// })
+
+let addResult;
+let mResult;
+let a = 1;
+let b = 10;
+let m1 = 1;
+let m2 = 30;
+
+function add(a, b, callback) {
+  addResult = a + b;
+  return addResult;
+}
+
+function multiply(a, b, callback) {
+  mResult = a * b;
+  return mResult;
+}
+
+add((a = 1), (b = 10), function() {
+  return addResults;
+  console.log(addResults);
+  //   multiply(addResults, m1, mResult => {
+  //     multiply(mResult, m2, _ => {
+  //       console.log(answer);
+  //     });
+  //   });
+});
