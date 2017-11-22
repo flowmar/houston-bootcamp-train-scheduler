63e223569641ce606819ab924806a4f78855f21a
Omar Imam <flowmar@kineticvortex.com>
2017-11-22 11:34:56 -0600

    Begin refactoring yucky code blocks into delicious functions

--- public/train.js
+++ public/train.js
@@ -1,14 +1,13 @@
-// import 'babel';
-// import * as firebase from 'firebase';
+"use strict";
 
 // Initialize Firebase
 var config = {
-  apiKey: 'AIzaSyBxb0Xg38kI4YclVCdetdlGRWU5dckB0sA',
-  authDomain: 'train-scheduler-216e6.firebaseapp.com',
-  databaseURL: 'https://train-scheduler-216e6.firebaseio.com',
-  projectId: 'train-scheduler-216e6',
-  storageBucket: 'train-scheduler-216e6.appspot.com',
-  messagingSenderId: '391022964912'
+  apiKey: "AIzaSyBxb0Xg38kI4YclVCdetdlGRWU5dckB0sA",
+  authDomain: "train-scheduler-216e6.firebaseapp.com",
+  databaseURL: "https://train-scheduler-216e6.firebaseio.com",
+  projectId: "train-scheduler-216e6",
+  storageBucket: "train-scheduler-216e6.appspot.com",
+  messagingSenderId: "391022964912"
 };
 
 firebase.initializeApp(config);
@@ -22,107 +21,121 @@ const trains = db.ref();
 // Create a global variable for the train data
 let trainData;
 let trainName;
-let destination;
-let frequency;
-let nextArrival;
-let minutesAway;
+// let destination;
+// let frequency;
+// let nextArrival;
+// let minutesAway;
 
-function loadpage() {
-  // Listen for changes to the database
-  trains.on('value', snapshot => {
-    // Prints the array of train objects to the console
-    trainData = snapshot.val();
-    trainName = trainData[0].trainName;
-    // console.log(trainData);
-    // console.log(trainName);
+// Clears out the user input boxes
+function clearBoxes() {
+  $("#trainName").val("");
+  $("#destination").val("");
+  $("#frequency").val("");
+  $("#firstTrainTime").val("");
+}
+
+// Creates a table based on the preexisting data in the database
+function createTable() {
+  // Loop through the data from Firebase and set the train attributes to variables
+  for (let i = 0; i < Object.keys(trainData).length; i++) {
+    let trainName = trainData[i].trainName;
+    let destination = trainData[i].destination;
+    let frequency = trainData[i].frequency;
+    let nextArrival = trainData[i].nextArrival;
+    let minutesAway = trainData[i].minutesAway;
+    let firstTrainTime = trainData[i].firstTrainTime;
+    console.log("Train Number: " + i);
+    console.log("Train Name: " + trainName);
+    console.log("Destination: " + destination);
+    console.log("Frequency: " + frequency);
 
     // Loop through the data and create a row in the HTML table for every entry
-    for (var i = 0; i < trainData.length; i++) {
-      trainName = trainData[i].trainName;
-      destination = trainData[i].destination;
+    $("#tbody").append(
+      '<tr><td class="font-weight-bold lead h1">' +
+        trainName +
+        '</td><td class="font-italic lead text-uppercase">' +
+        destination +
+        '</td><td class="lead">' +
+        frequency +
+        '</td><td class="lead">' +
+        nextArrival +
+        '</td><td class="lead">' +
+        minutesAway +
+        "</td></tr>"
+    );
+  }
 
-      frequency = trainData[i].frequency;
-      nextArrival = trainData[i].nextArrival;
-      minutesAway = trainData[i].minutesAway;
-      // console.log(trainName);
-      // console.log(destination);
-      $('#table').append(
-        '<tr><td class="font-weight-bold lead h1">' +
-          trainName +
-          '</td><td class="font-italic lead text-uppercase">' +
-          destination +
-          '</td><td class=lead>' +
-          frequency +
-          '</td><td class=lead>' +
-          nextArrival +
-          '</td><td class=lead>' +
-          minutesAway +
-          '</td></tr>'
-      );
-    }
-    var now = moment().format();
-    console.log(now);
-  });
+  // Use the firstTrainTime and frequency of each train to calculate the Next Arrival and Minutes Away columns
+  for (let j = 0; j < Object.keys(trainData).length; j++) {
+    firstTrainTime;
+  }
+}
 
+function setClock() {
   // Set an Interval to update the time at the top of the page every second
   setInterval(_ => {
-    let now = moment().format('LTS');
-    let nowDay = moment().format('LL');
+    let now = moment().format("LTS");
+    let nowDay = moment().format("LL");
 
-    $('#time').empty();
-    $('#time').append(
-      'The current time is:<br> <strong>' +
+    $("#time").empty();
+    $("#time").append(
+      "The current time is:<br> <strong>" +
         now +
-        '</strong>' +
-        '<br> on <br>' +
+        "</strong>" +
+        "<br> on <br>" +
         nowDay
     );
   }, 1000);
+}
 
-  var firstTrainTime = moment([2014, 5, 22, 12, 00, 01, 00]);
-  $('#test').html('<span class="text-white">' + firstTrainTime) + '</span>';
+function loadpage() {
+  // Listen for changes to the database, when a change occurs,
+  trains.on("value", snapshot => {
+    // Set the trainData variable to the human-readable value of the snapshot
+    trainData = snapshot.val();
+    console.log(trainData);
+    // Run the function to create the table from the preexisting data
+    createTable();
+    // Run the function to create the clock at the top of the screen
+    setClock();
+  });
 
   // When the 'submit' button is clicked...
-  $('#submit').on('click', function() {
+  $("#submit").on("click", function() {
     // Take the input from the input text boxes
-    var trainName = $('#trainName')
+    var trainName = $("#trainName")
       .val()
       .trim();
-    var destination = $('#destination')
+    var destination = $("#destination")
       .val()
       .trim();
-    var frequency = $('#frequency')
+    var frequency = $("#frequency")
       .val()
       .trim();
     var firstTrainTime = moment(
-      $('#firstTrainTime')
+      $("#firstTrainTime")
         .val()
         .trim(),
-      'HH:mm'
+      "HH:mm"
     )
-      .subtract(10, 'years')
-      .format('X');
+      .subtract(10, "years")
+      .format("X");
 
     let newTrain = {
-      name: trainName,
+      trainName: trainName,
       destination: destination,
       frequency: frequency,
-      firstTrain: firstTrainTime
+      firstTrainTime: firstTrainTime
     };
 
     // Push new train to the database
-    db.ref().push(newTrain);
+    trains.push(newTrain);
     clearBoxes();
 
-    alert('Success!');
+    alert("Success!");
   });
 }
 
-function clearBoxes() {
-  $('#trainName').val('');
-  $('#destination').val('');
-  $('#frequency').val('');
-  $('#firstTrainTime').val('');
-}
-
-loadpage();
+$(document).ready(function() {
+  loadpage();
+});
